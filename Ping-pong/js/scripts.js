05fc77dd04ab7dd172b109a073d22d3f61895c6f
Rouz Majlessi <rmajlessi@gmail.com>
2016-06-30 15:14:55 -0700

    debugged and fixed input names in js

--- Ping-pong/js/scripts.js
+++ Ping-pong/js/scripts.js
@@ -1,24 +1,26 @@
-$(document).ready(function) {
+$(document).ready(function() {
   $("form#Pong").submit(function(event) {
     event.preventDefault();
 
-    var number = pareInt($("input#number").val());
-    var output = countNumber (countNumber);
+    var number = parseInt($("input#userinput").val());
+    var output = numbers(number);//
     var display = "";
-
+      output.forEach(function(items){
+        $("#output").append("<p>"+items+" <p>");
+      });
   });
 });
 
 
-    fucntion numbers(number) {
+    function numbers(number) {
       var input = [];
-      for (var i = 1; i <= number, i += 1){
+      for (var i = 1; i <= number;i++) {//dwfewfrfrere
         if (i % 15 === 0) {
           input.push("ping pong");
         } else if (i % 3 === 0){
           input.push("ping");
         } else if (i % 5 === 0) {
-          input.push("pong")
+          input.push("pong");
         } else {
           input.push(i);
         }
