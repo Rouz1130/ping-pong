c4590137879cca2eb1cff01c35ac0f07adf8d9fe
Rouz Majlessi <rmajlessi@gmail.com>
2016-06-29 23:29:44 -0700

    add business logic

--- Ping-pong/js/scripts.js
+++ Ping-pong/js/scripts.js
@@ -2,14 +2,26 @@ $(document).ready(function) {
   $("form#Pong").submit(function(event) {
     event.preventDefault();
 
-    var number = $("input#number").val
+    var number = pareInt($("input#number").val());
     var output = countNumber (countNumber);
     var display = "";
 
-
-
-
-
-
   });
 });
+
+
+    fucntion numbers(number) {
+      var input = [];
+      for (var i = 1; i <= number, i += 1){
+        if (i % 15 === 0) {
+          input.push("ping pong");
+        } else if (i % 3 === 0){
+          input.push("ping");
+        } else if (i % 5 === 0) {
+          input.push("pong")
+        } else {
+          input.push(i);
+        }
+      }
+        return input;
+      };
