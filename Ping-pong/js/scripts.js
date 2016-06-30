d4750a9a515fcf3ea6d355e55ecfe0c347ce6183
Rouz Majlessi <rmajlessi@gmail.com>
2016-06-29 23:10:08 -0700

    add user interface logic in js

---
+++ Ping-pong/js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function) {
+  $("form#Pong").submit(function(event) {
+    event.preventDefault();
+
+    var number = $("input#number").val
+    var output = countNumber (countNumber);
+    var display = "";
+
+
+
+
+
+
+  });
+});
